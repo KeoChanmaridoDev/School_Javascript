b23998defa9ca5981a31df4c0b3f3ce08345b383
Keo chanmarido <Leooscar.psl@gmail.com>
2024-12-07 13:49:21 +0700

    main

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function toggleMenu() {
     document.getElementById('dropdown').classList.toggle('hidden');
+    
+}
+function toggleMenu1() {
+    document.getElementById('social').classList.toggle('hidden');
 }
 
 function hideAllCards() {
@@ -12,9 +16,11 @@ function hideAllCards() {
 
 function setec() {
     hideAllCards(); 
+    
     document.getElementById("setec").classList.remove("hidden");  
     document.getElementById('dropdown').classList.add('hidden'); 
-
+    document.getElementById("social-btn").classList.remove("hidden");
+    document.getElementById('blank').classList.toggle('hidden');
     document.getElementById("facebook").href = "https://www.facebook.com/setecedu";
     document.getElementById("telegram").innerHTML = "+855 12 395 190";
     document.getElementById("youtube").href = "https://www.youtube.com/@setecinstitute7131";
@@ -31,7 +37,7 @@ function rupp() {
     hideAllCards();  
     document.getElementById("rupp").classList.remove("hidden");  
     document.getElementById('dropdown').classList.add('hidden'); 
-    
+    document.getElementById("social-btn").classList.remove("hidden");
     document.getElementById("facebook").href = "https://www.facebook.com/profile.php?id=100083062174327";
     document.getElementById("telegram").innerHTML = "+855 023 883 640";
     document.getElementById("youtube").href = "https://www.youtube.com/@សមាគមនិស្សិតសាកលវិទ្យាល័យភូមិ";
@@ -48,7 +54,7 @@ function ace() {
     hideAllCards();  
     document.getElementById("ace").classList.remove("hidden");  
     document.getElementById('dropdown').classList.add('hidden');
-    
+    document.getElementById("social-btn").classList.remove("hidden");
     document.getElementById("facebook").href = "https://www.facebook.com/ace.cambodia";
     document.getElementById("telegram").innerHTML = "+855 023 222 325";
     document.getElementById("youtube").href = "https://www.youtube.com/@IDPCambodia";
